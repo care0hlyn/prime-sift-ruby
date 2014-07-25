d0b19639e8cc127f3e448f6f515a1fe8b49d90a3
Amoreena Hagedorn & Carolyn Huynh <student@epicodus.com>
2014-07-25 14:53:17 -0700

    All specs pass

--- spec/prime_sifting_spec.rb
+++ spec/prime_sifting_spec.rb
@@ -5,11 +5,16 @@ describe("prime_sifting") do
   it("returns 2 as a prime number") do
     prime_sifting(2).should(eq([2]))
   end
+
+  it("returns 3 as a prime number") do
+    prime_sifting(3).should(eq([2, 3]))
+  end
+
   it("returns a list of prime numbers less than the inputted number") do
     prime_sifting(3).should(eq([2, 3]))
   end
 
   it("returns a list of prime numbers less than the inputted number") do
-    prime_sifting(4).should(eq([2, 3]))
+    prime_sifting(7).should(eq([2, 3, 5, 7]))
   end
 end
